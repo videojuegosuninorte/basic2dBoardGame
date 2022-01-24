cffce649031197e30596d9cf5409c168dba2f508
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2022-01-24 12:17:13 -0500

    Add board

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -6,7 +6,8 @@ public class GameLogic : MonoBehaviour
 {
     public int width = 4;
     public int height = 4;
-    public Block blockPregab;
+    public Block blockPrefab;
+    public SpriteRenderer boardPrefab;
     void Start()
     {
         GenerateBoard();
@@ -18,17 +19,16 @@ public class GameLogic : MonoBehaviour
         {
             for (int j = 0; j < width; j++)
             {
-                var block = Instantiate(blockPregab, new Vector2(i, j), Quaternion.identity);
+                var block = Instantiate(blockPrefab, new Vector2(i, j), Quaternion.identity);
             }
         }
 
         var center = new Vector2((float)width / 2 - 0.5f, (float)height / 2 - 0.5f);
 
+        var board = Instantiate(boardPrefab, center, Quaternion.identity);
+        board.size = new Vector2(width, height);
+
         Camera.main.transform.position = new Vector3(center.x, center.y, -20);
     }
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Basic 2D Game 
+# Basic 2D Bord Game 
 
 <img src="demo.gif" width="300" />
